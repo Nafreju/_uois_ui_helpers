1b960ed2a84f181d43d70d33593c25b6d5196f3d
Nafreju <buresmichal98@seznam.cz>
2023-07-19 11:31:59 +0200

    comments

--- src/components/Tasks/TaskRow.js
+++ src/components/Tasks/TaskRow.js
@@ -3,18 +3,16 @@ import { TextAreaDelayed } from "components/TextAreaDelayed";
 import { DateInputChange } from "./TaskDateChange";
 import { formatDate } from "utils/DateUtils";
 import { TaskDetailedDesChange } from "./TaskDetailedDesChange";
-//komponenta řádky
-export const TaskRow = ({task, actions}) => {
-    const today = new Date().getTime();   //vytváření datumu a času
-    
-    const dateEntryTD = formatDate(task.dateOfEntry)
-    const dateSubmissionTD = formatDate(task.dateOfSubmission)
-    let deadline_color = "red";
-    if (today < new Date(dateSubmissionTD)) deadline_color = "green";
+import { TaskDateChange } from "./TaskDateChange";
+import { TaskReferenceChange } from "./TaskReferenceChange";
 
+//komponenta řádky
+export const TaskRow = ({task, actions, index}) => {
+    const today = new Date().getTime();   //vytváření datumu a času
+    const dateEntryTD = formatDate(task.dateOfEntry)
     const briefDesServerChange = (value) => {
       //add mutation to change atribute
-      actions.updateTask({...task, briefDes:value})
+    actions.updateTask({...task, briefDes:value})
     }
 //tabulka pro zadání úkolu uživateli a input 1 pro stručný popis úkolu
 //vrací id  uživatele, stručný popis úkolu, datum odevzdání a naplnění, detailní popis úkolu a odkaz
@@ -23,7 +21,6 @@ export const TaskRow = ({task, actions}) => {
         <th>{index + 1}</th>
         <td><TaskNameChange task={task} actions={actions}/></td>
         <td>{dateEntryTD}</td>
-        <td style={{color:deadline_color}}><TaskDateChange task={task} actions={actions}/></td>
         <td> <TextAreaDelayed key={task.id} placeholder={task.briefDes} value={task.briefDes} onChange={briefDesServerChange}/></td>
         <td> <TaskDetailedDesChange task={task} actions={actions}/></td>
         <td> <a href={task?.reference}>{task?.reference}</a><TaskReferenceChange task={task} actions={actions}/></td>
